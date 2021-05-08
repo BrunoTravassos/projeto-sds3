69857640303c4a87ef20914363cb4a9f041fdba4
BrunoTravassos <brunodefrontin@gmail.com>
2021-05-08 19:09:19 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,12 @@
+import { type } from "os"
+
 export type SaleSum = {
-        sum: number,
-        sellerName: string
-    }
+    sum: number;
+    sellerName: string;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
